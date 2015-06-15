f08b4d85425c6c05c6edbfdd0afd879a95c18e9b
Artūrs Liepiņš <arturs@liepins.me>
2015-06-15 14:26:38 +0000

    Add setup script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+# Run with `curl https://raw.githubusercontent.com/splenetic/dotfiles/master/setup.sh | bash`
+
+set -e
+REPO="https://github.com/splenetic/dotfiles.git"
+DOTDIR="$HOME/.dotfiles"
+if [ ! -d "$DOTDIR" ]; then
+    git clone $REPO "$DOTDIR"
+elif [ "$(cd "$DOTDIR" && git config --get remote.origin.url)" != "${REPO}" ]; then
+    >&2 echo "Unknown .dotfiles folder exists."
+    >&2 echo "Please delete and try again."
+    exit 1
+else
+    echo "Dotfiles found!"
+    echo "Updating..."
+    (cd "$DOTDIR" && git pull && git submodule update --init)
+fi
+
+pushd $HOME
+ln -sfv .dotfiles/.vim .vim
+ln -sfv .dotfiles/.vimrc .vimrc
+ln -sfv .dotfiles/.tmux.conf .tmux.conf
+popd
